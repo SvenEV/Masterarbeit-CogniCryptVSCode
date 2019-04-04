5b1379d4fb111249a7b926d51c3cb45d680a314d
Sven Vinkemeier <svenvinkemeier@live.de>
2019-04-04 16:22:11 +0200

    Added support for language server connection via sockets

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,24 +1,50 @@
 import { workspace, ExtensionContext, window } from 'vscode';
 
+import * as net from 'net';
 import {
 	LanguageClient,
 	LanguageClientOptions,
-	ServerOptions
+	ServerOptions,
+	StreamInfo
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
 	// Startup options for the language server
-	const serverOptions: ServerOptions = {
+	const lspTransport = workspace.getConfiguration().get("cognicrypt.lspTransport", "stdio");
+
+	const serverOptionsStdio = {
 		command: "java",
 		args: [
 			`-Duser.project=${context.extensionPath}/resources`,
 			"-jar",
 			`${context.extensionPath}/resources/crypto-lsp-demo-0.0.1-SNAPSHOT.jar`
 		]
-	};
-	
+	}
+
+	const serverOptionsSocket = () => {
+		const socket = net.connect({ port: 5007 });
+		const result: StreamInfo = {
+			writer: socket,
+			reader: socket
+		};
+		return new Promise<StreamInfo>((resolve, reject) => {
+			socket.on("connect", () => resolve(result));
+			socket.on("error", _ =>
+				window.showErrorMessage(
+					"Failed to connect to CogniCrypt language server. Make sure that the language server is running " +
+					"-or- configure the extension to connect via standard IO."));
+		})
+	}
+
+	const serverOptions: ServerOptions =
+		(lspTransport === "stdio") ? serverOptionsStdio :
+			(lspTransport === "socket") ? serverOptionsSocket : null;
+
+	if (!serverOptions)
+		throw Error("Unsupported configuration value for 'cognicrypt.lspTransport': " + lspTransport)
+
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for Java documents
@@ -29,7 +55,7 @@ export async function activate(context: ExtensionContext) {
 		synchronize: {
 			configurationSection: 'java',
 			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: [ workspace.createFileSystemWatcher('**/*.java') ]
+			fileEvents: [workspace.createFileSystemWatcher('**/*.java')]
 		}
 	};
 
